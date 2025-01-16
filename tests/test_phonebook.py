31b64906dc27ca4b15696dead25d04a6680c25f8
Lokesh Keyan <lokeshkeyanuf@gmail.com>
2025-01-16 13:35:53 -0500

    Add new tests and test configuration for vscode

--- tests/test_phonebook.py
+++ tests/test_phonebook.py
@@ -1,4 +1,5 @@
 import unittest
+
 from UnitTesting.phonebook.phonebook import PhoneBook
 
 class PhoneBookTest(unittest.TestCase):
@@ -7,3 +8,8 @@ class PhoneBookTest(unittest.TestCase):
         phonebook.add("Bob", "12345")
         number = phonebook.lookup("Bob")
         self.assertEqual("12345", number)
+    
+    def test_missing_name(self):
+        phonebook = PhoneBook()
+        with self.assertRaises(KeyError):
+            phonebook.lookup("missing")
