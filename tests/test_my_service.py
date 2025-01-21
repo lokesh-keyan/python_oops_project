458c1787098d993dbf8274dcd0496b7f3cbbcdce
Lokesh Keyan <lokeshkeyanuf@gmail.com>
2025-01-21 11:54:43 -0500

    Mock test

--- tests/test_my_service.py
+++ tests/test_my_service.py
@@ -1,4 +1,6 @@
 from unittest.mock import Mock
+
+import pytest
 from SingleSignOn.my_service import MyService, Request
 from SingleSignOn.single_sign_on import SSOToken, SingleSignOnRegistry
 
@@ -14,3 +16,19 @@ def test_single_sign_on():
     token = SSOToken
     service.handle(Request("Alice"), token)
     spy_sso_registry.is_valid.assert_called_with(token)
+
+def confirm_token(token):
+    def is_valid(actual_token):
+        if actual_token != token:
+            raise ValueError("wrong token received")
+    
+    return is_valid
+
+def test_single_sign_on_receives_correct_token():
+    mock_sso_resgistry = Mock(SingleSignOnRegistry)
+    correct_token = SSOToken
+    mock_sso_resgistry.is_valid = Mock(side_effect=confirm_token(correct_token))
+    service = MyService(mock_sso_resgistry)
+    wrong_token = SSOToken
+    service.handle(Request("Alice"), wrong_token)
+    mock_sso_resgistry.is_valid.assert_called_with(correct_token)
